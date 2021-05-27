6370a01c087a07686e2993bde498762a72ee0a03
siryus1713 <76882969+siryus1713@users.noreply.github.com>
2021-05-27 23:11:50 +0800

    Create oper.h

---
+++ oper.h
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <math.h>
+
+
+int get_size(char *);
+
+
+void swap(const int *,const int *);
+
+tn *init(int );
+
+
+ll *list(int);
+
+
+ll *l_insert(ll *,int,int *);
+
+
+void build_array(int *,int ,int *);
+
+
+void query_array(int *,int *);
+
+
+void selection_sort(int *,int );
+
+
+void build_bs_arr(int *,int );
+
+
+void binary_search(int *,int ,int );
+
+
+
+void query_bs(int *,int ,int *);
+
+
+ll *build_ll(ll *,int );
+
+
+void querry_link_list(ll *,int ,int *);
+
+
+tn *build_bst(tn *,int );
+
+
+void search_bst(tn *,int );
+
+
+
+void querry_bst(tn *,int ,int *);
